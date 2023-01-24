a51411efb32567664b96546976e3231ecf0f264e
eunbum <icesnake72@gmail.com>
2023-01-24 22:56:25 +0900

    추상 메소드 사용

--- src/MyPack/MyChampion.java
+++ src/MyPack/MyChampion.java
@@ -1,6 +1,6 @@
 package MyPack;
 
-public class MyChampion {
+public abstract class MyChampion {
 
     class Location {
         private int x;
@@ -23,6 +23,7 @@ public class MyChampion {
 
     public static int lastId = 0;
     private int HP;
+    private int SP;
     private int AttackPower;
 
     private String name;
@@ -73,6 +74,7 @@ public class MyChampion {
     public void GetInfo() {
         System.out.println("Information of " + this);
         System.out.println("HP : " + HP);
+        System.out.println("SP : " + SP);
         System.out.println("X : " + loc.x + " Y : " + loc.y);
     }
 
@@ -101,28 +103,88 @@ public class MyChampion {
         HP += weight;
         System.out.println(this + "의 HP가 " + weight + "만큼 회복되어 " + HP + " 입니다");
     }
+
+    protected void InitSP(int SP) {
+        this.SP = SP;
+    }
+
+    protected void IncSP(int weight) {
+        SP += weight;
+        System.out.println(this + "의 SP가 " + weight + "만큼 회복되어 " + SP + " 입니다");
+    }
+
+    protected void DecSP(int weight) {
+        SP -= weight;
+        System.out.println(this + "의 SP가 " + weight + "만큼 감소되어 " + SP + " 입니다");
+    }
+
+    protected void DecHP(int power) {
+        HP -= power;
+        System.out.println(this + "의 HP가 " + power + "만큼 감소되어 " + HP + " 입니다");
+    }
+
+    public abstract void Skill(MyChampion target);
 }
 
 class Kaisa extends MyChampion {
+    public static final int KAISA_FULL_HP = 300;
     public Kaisa() {
-        super("카이사", 300, 10);
+        super("카이사", KAISA_FULL_HP, 10);
+        InitSP(100);
     }
 
     @Override
     public void Attack(MyChampion enemy) {
         super.Attack(enemy);
+
         SelfHeal(2);
+    }
+
+    @Override
+    public void Skill(MyChampion target) {
+        System.out.println(this + "가 " + target + "에게 스킬을 사용함");
+        target.DecHP(50);
+        target.DecSP(5);
+
     }
 }
 
 class Zed extends MyChampion {
+    public static final int ZED_FULL_HP = 500;
     public Zed() {
-        super("제드", 500, 20);
+        super("제드", ZED_FULL_HP, 20);
+        InitSP(50);
+    }
+
+    @Override
+    public void Attack(MyChampion enemy) {
+        super.Attack(enemy);
+        SelfHeal(-1);
+    }
+
+    @Override
+    public void Skill(MyChampion target) {
+        System.out.println(this + "가 " + target + "에게 스킬을 사용함");
+        target.DecHP(100);
     }
 }
 
 class Lux extends MyChampion {
+    public static final int LUX_FULL_HP = 300;
     public Lux() {
-        super("럭스", 300, 5);
+        super("럭스", LUX_FULL_HP, 2);
+        InitSP(150);
+    }
+
+    @Override
+    public void Attack(MyChampion enemy) {
+        super.Attack(enemy);
+        SelfHeal(3);
+    }
+
+    @Override
+    public void Skill(MyChampion target) {
+        System.out.println(this + "가 " + target + "에게 스킬을 사용함");
+        target.SelfHeal(10);
     }
 }
